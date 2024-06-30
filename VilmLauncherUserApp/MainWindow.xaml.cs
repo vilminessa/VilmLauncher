0e11bf8238402c0187fbad20a8d1666a5f483844
vilminessa <vilminessa@gmail.com>
2024-06-30 09:37:22 +0600

    добавил приложение для тестов, установил библиотеки (AutoMapper(попытался по крайней мере), Microsoft.AspNetCore.Mvc.Core(с зависимостями), Microsoft.EntityFrameworkCore(с зависимостями), Moq, Moq.EntityFrameworkCore(Попытался), NLog, Hangfire(с зависимостями), MediatR, Swashbuckle(с зависимостями))

---
+++ VilmLauncherUserApp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VilmLauncherUserApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int aga = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click_1(object sender, RoutedEventArgs e)
+        {
+            aga = aga + 1;
+            string agaS = aga.ToString();
+            TextBox1.Text = agaS;
+        }
+    }
+
+}
